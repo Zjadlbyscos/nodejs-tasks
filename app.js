9a0d6a9329bf41af5f4a5c3c4d2fe6dd1747b1dd
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-01-29 19:44:54 +0100

    add avatars  / path saving/

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-import mongoose from "mongoose";
-import express from "express";
-import passport from "./config/passport.js";
-import {router as authRoutes} from "./routes/api/auth.routes.js";
-import { router as contactRoutes } from "./routes/api/contacts.routes.js";
-
-
+import path from "path";
 import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import passport from "./config/passport.js";
+import { router as authRoutes } from "./routes/api/auth.routes.js";
+import { router as contactRoutes } from "./routes/api/contacts.routes.js";
+import createFolderIfNotExist from "./helpers/multer.js";
+
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 4000;
+
+const __dirname = path.resolve();
+const publicDir = path.join(__dirname, "public");
+const uploadDir = path.join(__dirname, "public/tmp");
+const storeImage = path.join(__dirname, "public/avatars");
 
 const connection = mongoose.connect(process.env.DB_URL, {
   dbName: "db-contacts",
@@ -25,6 +31,9 @@ connection
   .then(() => {
     console.log("Database connection successful");
     app.listen(PORT, () => {
+      createFolderIfNotExist(publicDir);
+      createFolderIfNotExist(uploadDir);
+      createFolderIfNotExist(storeImage);
       console.log(`App listens on port ${PORT}`);
     });
   })
