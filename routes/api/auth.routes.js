a9846b36bfb797cbb431f58b244c6a50e92cd157
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-02-05 13:28:17 +0100

    style: Format code with Prettier

--- routes/api/auth.routes.js
+++ routes/api/auth.routes.js
@@ -4,7 +4,10 @@ import authController from "../../controllers/auth.controller.js";
 import auth from "../../middlewares/auth.js";
 import uploadAvatar from "../../controllers/multer.controller.js";
 import validateUpload from "../../middlewares/avatarValidateUpload.js";
-import { verifyEmail, resendVerification } from "../../controllers/email.controller.js";
+import {
+  verifyEmail,
+  resendVerification,
+} from "../../controllers/email.controller.js";
 
 const router = express.Router();
 
@@ -12,7 +15,7 @@ router.post("/login", authController.login);
 router.post("/signup", authController.signup);
 router.get("/logout", auth, authController.logout);
 router.get("/current", auth, authController.current);
-router.patch("/avatars",auth,validateUpload, uploadAvatar)
+router.patch("/avatars", auth, validateUpload, uploadAvatar);
 
 router.get("/verify/:verificationToken", verifyEmail);
 router.post("/verify", resendVerification);
