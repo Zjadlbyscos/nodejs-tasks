21226ebb0534b6ddcddbcd80b41fc7088e753ec4
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-02-06 13:27:28 +0100

    fix: Ensure async email snd in resendVerification

--- controllers/email.controller.js
+++ controllers/email.controller.js
@@ -2,67 +2,63 @@ import User from "../user/user.model.js";
 import sendEmail from "../helpers/mailer.js";
 
 const verifyEmail = async (req, res, _) => {
-    const { verificationToken } = req.params;
-    const user = await User.findOne({ verificationToken });
-    if (!user) {
-      return res.json({
-        status: "Error",
-        code: 404,
-        message: "User not found",
-      });
-    }
-    user.verify = true;
-    user.verificationToken = null;
-    await user.save();
+  const { verificationToken } = req.params;
+  const user = await User.findOne({ verificationToken });
+  if (!user) {
     return res.json({
-      status: "OK",
-      code: 200,
-      message: "Verification successful",
+      status: "Error",
+      code: 404,
+      message: "User not found",
     });
-  };
-  
-  const resendVerification = async (req, res, _) => {
-    const { email } = req.body;
-    if (!email) {
-      return res.json({
-        status: "Error",
-        code: 400,
-        message: "Missing required field: email",
-        data: "Bad Request",
-      });
-    }
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.json({
-        status: "Error",
-        code: 404,
-        message: "User not found",
-      });
-    }
-    if (user.verify) {
-      return res.json({
-        status: "Error",
-        code: 400,
-        message: "Verification already completed",
-        data: "Bad Request",
-      });
-    }
-    const verificationLink = `${req.protocol}://${req.get(
-      "host"
-    )}/api/users/verify/${user.verificationToken}`;
-    sendEmail({
-      to: email,
-      link: verificationLink,
-    });
+  }
+  user.verify = true;
+  user.verificationToken = null;
+  await user.save();
+  return res.json({
+    status: "OK",
+    code: 200,
+    message: "Verification successful",
+  });
+};
+
+const resendVerification = async (req, res, _) => {
+  const { email } = req.body;
+  if (!email) {
     return res.json({
-      status: "OK",
-      code: 200,
-      message: "Verification email resent",
+      status: "Error",
+      code: 400,
+      message: "Missing required field: email",
+      data: "Bad Request",
     });
-  };
-  
-  export {
-    verifyEmail,
-    resendVerification,
-  };
-  
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.json({
+      status: "Error",
+      code: 404,
+      message: "User not found",
+    });
+  }
+  if (user.verify) {
+    return res.json({
+      status: "Error",
+      code: 400,
+      message: "Verification already completed",
+      data: "Bad Request",
+    });
+  }
+  const verificationLink = `${req.protocol}://${req.get("host")}users/verify/${
+    user.verificationToken
+  }`;
+  await sendEmail({
+    to: email,
+    link: verificationLink,
+  });
+  return res.json({
+    status: "OK",
+    code: 200,
+    message: "Verification email resent",
+  });
+};
+
+export { verifyEmail, resendVerification };
